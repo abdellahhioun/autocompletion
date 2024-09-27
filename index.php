d7f2119810ed17149755604114e98312435f2f14
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-27 09:35:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Search Engine</title>
-  <link rel="stylesheet" href="styles/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recherche Pokémon</title>
 </head>
 <body>
-  <header>
-    <h1>Search Engine</h1>
-    <form action="recherche.php" method="GET">
-      <input type="text" id="search-input" name="search" placeholder="Search..." autocomplete="off">
-      <div id="autocomplete-list"></div>
+    <h1>Bienvenue sur le moteur de recherche Pokémon</h1>
+    <form action="recherche.php" method="get">
+        <input type="text" name="search" placeholder="Recherchez un Pokémon...">
+        <button type="submit">Rechercher</button>
     </form>
-  </header>
-
-  <script src="js/autocomplete.js"></script>
 </body>
 </html>
